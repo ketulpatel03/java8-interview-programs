294784b6c41d8f4c22f7d0ff45bf1883cd8df10e
ketulpatel03 <ketulpatel03@yahoo.co.in>
2024-05-22 17:12:04 -0600

    String decompress program optimized

--- src/main/java/com/java/tellTheOutput/stringDecompression/PSVMClass.java
+++ src/main/java/com/java/tellTheOutput/stringDecompression/PSVMClass.java
@@ -7,27 +7,19 @@ import java.util.Map;
 public class PSVMClass {
 
     public static void main(String[] args) {
-
         String str = "w4a3d1e1x6";
-        Map<Character, Integer> map = new LinkedHashMap<>();
+        StringBuilder strBuilder = new StringBuilder();
+
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             if (Character.isDigit(c)) {
-                System.out.println((c - '0') + "-" + str.charAt(i - 1));
-                map.put(str.charAt(i - 1), (c - '0'));
-            }
-        }
-        System.out.println("LinkedHashMap: " + map);
-
-        StringBuilder strBuilder = new StringBuilder();
-        for (Map.Entry m : map.entrySet()) {
-            Integer key = (Integer) m.getValue();
-            while (key != 0) {
-                strBuilder.append(m.getKey());
-                key--;
+                int j = (c - '0');
+                while (j != 0) {
+                    strBuilder.append(str.charAt(i - 1));
+                    j--;
+                }
             }
         }
         System.out.println("Output String: " + strBuilder);
     }
-
 }
